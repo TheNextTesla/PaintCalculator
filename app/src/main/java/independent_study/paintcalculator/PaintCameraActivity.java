51fc96b77f12ca010d9fd216ad6a2a702ab4a4e1
Blaine Huey <bhthenexttesla@gmail.com>
2018-03-22 13:40:25 -0400

    Starting Setup of Camera Activity

---
+++ app/src/main/java/independent_study/paintcalculator/PaintCameraActivity.java
@@ -0,0 +1,20 @@
+package independent_study.paintcalculator;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class PaintCameraActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
