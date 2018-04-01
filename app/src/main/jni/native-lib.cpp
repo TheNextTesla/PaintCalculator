094f97fc890c0dc94bb3bff16b9c23aee0a4775c
Blaine Huey <bhthenexttesla@gmail.com>
2018-04-01 13:47:52 -0400

    Hopefully Now Fully Functioning OpenCV Backend

---
+++ app/src/main/jni/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+
+JNIEXPORT void JNICALL Java_independent_study_paintcalulator_NativeBridge_drawBox(JNIEnv* env)
+{
+
+}
