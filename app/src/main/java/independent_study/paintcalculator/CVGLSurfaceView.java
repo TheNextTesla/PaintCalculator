8b49a11df3b95fad44d45fb4b341e6313a408c62
Blaine Huey <bhthenexttesla@gmail.com>
2018-04-04 13:21:01 -0400

    Overcomplicated and Hopefully Not Crashing Way of Getting Focal Length

--- app/src/main/java/independent_study/paintcalculator/CVGLSurfaceView.java
+++ app/src/main/java/independent_study/paintcalculator/CVGLSurfaceView.java
@@ -2,14 +2,16 @@ package independent_study.paintcalculator;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraCharacteristics;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.SizeF;
 import android.view.SurfaceHolder;
 
 import org.opencv.android.CameraGLSurfaceView;
 import org.opencv.core.Rect;
 
-public class CVGLSurfaceView extends CameraGLSurfaceView implements CameraGLSurfaceView.CameraTextureListener
+public class CVGLSurfaceView extends CameraGLSurfaceViewImproved implements CameraGLSurfaceViewImproved.CameraTextureListener
 {
     private static final String LOG_TAG = "CVGLSurfaceView";
 
@@ -18,12 +20,34 @@ public class CVGLSurfaceView extends CameraGLSurfaceView implements CameraGLSurf
     public CVGLSurfaceView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        
+        /*
         Camera c = Camera.open();
         focal_length = c.getParameters().getFocalLength();
         sizeW = Math.tan(c.getParameters().getHorizontalViewAngle()/2)*2*c.getParameters().getFocalLength();
         sizeH = Math.tan(c.getParameters().getVerticalViewAngle()/2)*2*c.getParameters().getFocalLength();
         c.release();
-        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        */
+
+        float[] focalLengths = getCameraCharacteristics().get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS);
+
+        if(focalLengths != null && focalLengths.length > 0)
+            focal_length = focalLengths[0];
+        else
+            focal_length = Double.NaN;
+
+        SizeF physicalSize = getCameraCharacteristics().get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE);
+
+        if(physicalSize != null)
+        {
+            sizeH = physicalSize.getHeight();
+            sizeW = physicalSize.getWidth();
+        }
+        else
+        {
+            sizeH = Double.NaN;
+            sizeW = Double.NaN;
+        }
     }
 
     @Override
@@ -59,12 +83,11 @@ public class CVGLSurfaceView extends CameraGLSurfaceView implements CameraGLSurf
         Log.d(LOG_TAG, "Width " + width + " Height " + height);
         //NativeBridge.testDraw(texIn, texOut, width, height);
 
-        Rect wallBlob = NativeBridge.blobAnalyze(texIn, texOut, width, height, 0, 255, 0, 255, 0, 255);
-        Log.d(LOG_TAG, "X " + wallBlob.x + " Y " + wallBlob.y);
+        //Rect wallBlob = NativeBridge.blobAnalyze(texIn, texOut, width, height, 0, 255, 0, 255, 0, 255);
+        //Log.d(LOG_TAG, "X " + wallBlob.x + " Y " + wallBlob.y);
 
-
-        //return true;
-        return false;
+        return true;
+        //return false;
     }
 
     public double calculateArea(Rect obj, double distance, int width, int height){
